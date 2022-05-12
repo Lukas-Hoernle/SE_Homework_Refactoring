dee1fdb4bb458744682cf47c7bafa54bb0efaf5d
Lukas <lukas.hoernle@web.de>
2022-05-12 11:14:07 +0200

    extracted methods and removed ugly comments

--- src/Customer.java
+++ src/Customer.java
@@ -18,8 +18,8 @@ class Customer {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();
-        StringBuilder resultBuilder = new StringBuilder().append("Rental Record for ").append(this.getName()).append("\n");
-        resultBuilder.append("\t").append("Title").append("\t").append("\t").append("Days").append("\t").append("Amount").append("\n");
+        StringBuilder resultBuilder = new StringBuilder();
+        addHeader(resultBuilder);
 
 
         while (enum_rentals.hasMoreElements()) {
@@ -29,16 +29,22 @@ class Customer {
             resultBuilder.append("\t").append(rental.getMovie().getTitle()).append("\t").append("\t").append(rental.getDaysRented()).append("\t").append(String.valueOf(amountPerLine)).append("\n");
             totalAmount += amountPerLine;
         }
+        addFooter(totalAmount, frequentRenterPoints, resultBuilder);
+        return resultBuilder.toString();
+    }
 
-        //add footer lines
+    private void addHeader(StringBuilder resultBuilder) {
+        resultBuilder.append("Rental Record for ").append(this.getName()).append("\n");
+        resultBuilder.append("\t").append("Title").append("\t").append("\t").append("Days").append("\t").append("Amount").append("\n");
+    }
+
+    private void addFooter(double totalAmount, int frequentRenterPoints, StringBuilder resultBuilder) {
         resultBuilder.append("Amount owed is ").append(totalAmount).append("\n");
         resultBuilder.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
-        return resultBuilder.toString();
     }
 
     private int getFrequentRenterPoints(int frequentRenterPoints, Rental rental) {
         frequentRenterPoints++;
-        // add bonus for a two day new release rental
         if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
             frequentRenterPoints++;
         return frequentRenterPoints;
